1a0c52473660872056be3d342f17fd79149fb070
me_nyong <80142915+MinCodeHub@users.noreply.github.com>
2024-07-19 18:08:33 +0900

    add: rememberPhoto

--- todaktodak/rememberTree/admin.py
+++ todaktodak/rememberTree/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import rememberTree
+from .models import rememberTree,Photo
 
 # Register your models here.
 admin.site.register(rememberTree)
+admin.site.register(Photo)
--- todaktodak/rememberTree/serializers.py
+++ todaktodak/rememberTree/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework.serializers import ModelSerializer
-from .models import rememberTree
+from .models import rememberTree, Photo
+
 
 class RememberSerializer(ModelSerializer):
     class Meta:
         model = rememberTree
         fields = ['id', 'treeName', 'myName', 'flowerType', 'growth_period']
+
+
+class PhotoSerializer(ModelSerializer):
+    class Meta:
+        model = Photo
+        fields =['id', 'rememberPhoto','description','rememberDate','comment','remember_tree']
