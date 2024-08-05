c16dfd3dcf15e8df0fb708ba00dece7c9cdf3883
SongYerim <102975547+SongYerim@users.noreply.github.com>
2024-08-05 22:20:03 +0900

    Update settings.py

--- todaktodak/todaktodak/settings.py
+++ todaktodak/todaktodak/settings.py
@@ -63,14 +63,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware', 
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-	'django.middleware.common.CommonMiddleware',
+    'django.middleware.common.CommonMiddleware',
     "allauth.account.middleware.AccountMiddleware", 
 ]
 
