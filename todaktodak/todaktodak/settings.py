52bd22c9e19ff7249561656f5af96e4ecdf2f50c
Yerim <yerim0307@gmail.com>
2024-08-06 04:14:00 +0900

    change URL

--- todaktodak/todaktodak/settings.py
+++ todaktodak/todaktodak/settings.py
@@ -79,7 +79,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'statifiles'),
+            os.path.join(BASE_DIR, 'client'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
