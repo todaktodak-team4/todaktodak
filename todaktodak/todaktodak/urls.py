e678cfdc1109c3bcb0f7f8f6a37409e580342cbf
Yerim <yerim0307@gmail.com>
2024-08-05 18:45:14 +0900

    change index path

--- todaktodak/todaktodak/urls.py
+++ todaktodak/todaktodak/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('',include('rememberTree.urls')),
     path('', include('memorialHall.urls')),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}), 
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
+    path('', TemplateView.as_view(template_name='index.html')),
     re_path(r'^assets/(?P<path>.*)$', serve, { 'document_root': settings.STATIC_ROOT }),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
