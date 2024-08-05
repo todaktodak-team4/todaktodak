731e86dbb3b011493f687f4eae77264242421450
yoon <sooin0105@gmail.com>
2024-08-05 19:02:54 +0900

    막

--- todaktodak/todaktodak/settings.py
+++ todaktodak/todaktodak/settings.py
@@ -79,7 +79,7 @@ ROOT_URLCONF = 'todaktodak.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-         'DIRS': ['client'],
+         'DIRS':[os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
